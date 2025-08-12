9d5401361f4b25d1d56a280e0f27a4e10d719eb4
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-08-12 16:22:17 +0700

    [DOC] Create changelog view from github commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\DocsController;
 use Dedoc\Scramble\Scramble;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/changelog', [DocsController::class, 'changelog']);
+
 
 Route::redirect('/', 'docs');
 
