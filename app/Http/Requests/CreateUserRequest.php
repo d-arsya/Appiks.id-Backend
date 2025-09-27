21ed619a13b3f06762f47790f4c97bf73acfe3d1
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-09-27 13:15:33 +0700

    [FIX] Class loader

--- app/Http/Requests/CreateUserRequest.php
+++ app/Http/Requests/CreateUserRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class CreateUserRequest extends FormRequest
 {
