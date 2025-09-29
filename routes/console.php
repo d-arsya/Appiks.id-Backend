712e408df5f5aacafc50051b1b31fc01d31e1b0e
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-09-29 19:23:50 +0700

    [FIX] Generate only hourly

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('que:work --stop-when-empty')->timezone('Asia/Jakarta')->everySecond();
 Schedule::command('compress:thumbnail')->everyTenSeconds();
-Schedule::command('generate:archtype')->everyTenSeconds();
+Schedule::command('generate:archtype')->hourly();
