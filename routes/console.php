47cf9906919f5a1d707c5a3c92b320ecb1a375d7
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-12-26 17:36:29 +0700

    [FIX] Remove queue worker

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('que:work --stop-when-empty')->timezone('Asia/Jakarta')->everySecond();
 Schedule::command('compress:thumbnail')->everyTenSeconds();
 Schedule::command('generate:archtype')->hourly();
